3c393e4d12452baa0766e8ea7c0a144e5e0f6301
Markus Winter <markus@wntr.me>
2018-02-07 14:48:10 +0100

    Simplifies point arithmetics and adds additional operators

--- Sources/Point.swift
+++ Sources/Point.swift
@@ -80,8 +80,21 @@ extension Point: CustomDebugStringConvertible where Numeric: CustomDebugStringCo
     public var debugDescription: String {
         return "Point<\(String(describing: Numeric.self))>(x: \(String(describing: x)), y: \(String(describing: y)))"
     }
+}
+
+// MARK: - Arithmetic
+
+extension Point: BasicArithmetic {
 
     /// Adds two values and stores the result in the left-hand-side variable.
+    ///
+    /// Example:
+    /// ```
+    ///     var p = Point(x: 1, y: 2)
+    ///     p += Point(x: 1, y: 2)
+    ///     // p.x == 2
+    ///     // p.y == 4
+    /// ```
     ///
     /// - Parameters:
     ///   - lhs: The first value to add.
@@ -91,25 +104,15 @@ extension Point: CustomDebugStringConvertible where Numeric: CustomDebugStringCo
         lhs.y += rhs.y
     }
 
-    /// Subtracts one value from another and produces their difference.
+    /// Subtracts two values and stores the result in the left-hand-side variable.
     ///
     /// Example:
     /// ```
-    /// let p1 = Point(x: 2, y: 4)
-    /// let p2 = Point(x: 0, y: 3)
-    /// let p3 = p1 - p2
-    /// /// p3.x == 2
-    /// /// p3.y == 1
+    ///     var p = Point(x: 2, y: 4)
+    ///     p -= Point(x: 0, y: 3)
+    ///     // p.x == 2
+    ///     // p.y == 1
     /// ```
-    ///
-    /// - Parameters:
-    ///   - lhs: A numeric value.
-    ///   - rhs: The value to subtract from `lhs`.
-    public static func - (lhs: Point, rhs: Point) -> Point {
-        return Point(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
-    }
-
-    /// Subtracts two values and stores the result in the left-hand-side variable.
     ///
     /// - Parameters:
     ///   - lhs: A numeric value.
@@ -119,25 +122,15 @@ extension Point: CustomDebugStringConvertible where Numeric: CustomDebugStringCo
         lhs.y -= rhs.y
     }
 
-    /// Multiplies two values and produces their product.
+    /// Multiplies two values and stores the result in the left-hand-side variable.
     ///
     /// Example:
     /// ```
-    /// let p1 = Point(x: 2, y: 4)
-    /// let p2 = Point(x: 1, y: 2)
-    /// let p3 = p1 * p2
-    /// /// p3.x == 2
-    /// /// p3.y == 8
+    ///     var p = Point(x: 2, y: 4)
+    ///     p *= Point(x: 1, y: 2)
+    ///     // p.x == 2
+    ///     // p.y == 8
     /// ```
-    ///
-    /// - Parameters:
-    ///   - lhs: The first value to multiply.
-    ///   - rhs: The second value to multiply.
-    public static func * (lhs: Point, rhs: Point) -> Point {
-        return Point(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
-    }
-
-    /// Multiplies two values and stores the result in the left-hand-side variable.
     ///
     /// - Parameters:
     ///   - lhs: The first value to multiply.
@@ -148,23 +141,167 @@ extension Point: CustomDebugStringConvertible where Numeric: CustomDebugStringCo
     }
 }
 
+// MARK: Extended
+
+extension Point: ExtendedArithmetic where Numeric: BinaryInteger {
+
+    /// Divides the first value by the second and stores the quotient in the
+    /// left-hand-side variable.
+    ///
+    /// For integer types, any remainder of the division is discarded.
+    ///
+    ///     var x = 21
+    ///     x /= 5
+    ///     // x == 4
+    ///
+    /// - Parameters:
+    ///   - lhs: The value to divide.
+    ///   - rhs: The value to divide `lhs` by. `rhs` must not be zero.
+    public static func /= (lhs: inout Point, rhs: Point) {
+        lhs.x /= rhs.x
+        lhs.y /= rhs.y
+    }
+
+    /// Divides the first value by the second and stores the remainder in the
+    /// left-hand-side variable.
+    ///
+    /// The result has the same sign as `lhs` and is less than `rhs.magnitude`.
+    ///
+    ///     var x = 22
+    ///     x %= 5
+    ///     // x == 2
+    ///
+    ///     var y = 22
+    ///     y %= -5
+    ///     // y == 2
+    ///
+    ///     var z = -22
+    ///     z %= -5
+    ///     // z == -2
+    ///
+    /// - Parameters:
+    ///   - lhs: The value to divide.
+    ///   - rhs: The value to divide `lhs` by. `rhs` must not be zero.
+    public static func %= (lhs: inout Point, rhs: Point) {
+        lhs.x %= rhs.x
+        lhs.y %= rhs.y
+    }
+}
+
+extension Point where Numeric: BinaryFloatingPoint {
+
+    /// Divides the first value by the second and stores the quotient in the
+    /// left-hand-side variable.
+    ///
+    /// For integer types, any remainder of the division is discarded.
+    ///
+    ///     var x = 21
+    ///     x /= 5
+    ///     // x == 4
+    ///
+    /// - Parameters:
+    ///   - lhs: The value to divide.
+    ///   - rhs: The value to divide `lhs` by. `rhs` must not be zero.
+    public static func /= (lhs: inout Point, rhs: Point) {
+        lhs.x /= rhs.x
+        lhs.y /= rhs.y
+    }
+
+    /// Divides the first value by the second and stores the remainder in the
+    /// left-hand-side variable.
+    ///
+    /// The result has the same sign as `lhs` and is less than `rhs.magnitude`.
+    ///
+    ///     var x = 22
+    ///     x %= 5
+    ///     // x == 2
+    ///
+    ///     var y = 22
+    ///     y %= -5
+    ///     // y == 2
+    ///
+    ///     var z = -22
+    ///     z %= -5
+    ///     // z == -2
+    ///
+    /// - Parameters:
+    ///   - lhs: The value to divide.
+    ///   - rhs: The value to divide `lhs` by. `rhs` must not be zero.
+    public static func %= (lhs: inout Point, rhs: Point) {
+        lhs.x.formTruncatingRemainder(dividingBy: rhs.x)
+        lhs.y.formTruncatingRemainder(dividingBy: rhs.y)
+    }
+}
+
+// MARK: Overflow
+
+extension Point: OverflowArithmethic where Numeric: FixedWidthInteger {
+
+    /// Returns the sum of the two given values, discarding any overflow.
+    ///
+    /// The masking addition operator (`&+`) silently discards any overflow that
+    /// occurs during the operation.
+    ///
+    /// Example:
+    /// ```
+    ///     let a = Point<Int8>(x: 5, y: 1) &+ Point<Int8>(x: 10, y: 3)
+    ///     // a.x == 10; a.y == 4
+    ///     let b = Point<Int8>(x: 20, y: 100) &+ Point<Int8>(x: 64, y: 121)
+    ///     // b.x == 84; b.y == -35 (after overflow)
+    /// ```
+    ///
+    ///
+    /// - Parameters:
+    ///   - lhs: The first value to add.
+    ///   - rhs: The second value to add.
+    public static func &+ (lhs: Point, rhs: Point) -> Point {
+        return Point(x: lhs.x &+ rhs.x, y: lhs.y &+ rhs.y)
+    }
+
+    /// Returns the difference of the two given values, discarding any overflow.
+    ///
+    /// The masking subtraction operator (`&-`) silently discards any overflow
+    /// that occurs during the operation.
+    ///
+    /// Example:
+    /// ```
+    ///     let a = Point<UInt8>(x: 21, y: 10) &- Point<Int8>(x: 10, y: 2)
+    ///     // a.x == 11; a.y == 8
+    ///     let b = Point<Int8>(x: 10, y: 10) &- Point<Int8>(x: 2, y: 21)
+    ///     // b.x == 8; b.y == 245 (after overflow)
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - lhs: A numeric value.
+    ///   - rhs: The value to subtract from `lhs`.
+    public static func &- (lhs: Point, rhs: Point) -> Point {
+        return Point(x: lhs.x &- rhs.x, y: lhs.y &- rhs.y)
+    }
+
+    /// Returns the product of the two given values, discarding any overflow.
+    ///
+    /// The masking multiplication operator (`&*`) silently discards any overflow
+    /// that occurs during the operation.
+    ///
+    /// Example:
+    /// ```
+    ///     let a = Point<Int8>(x: 2, y: 4) &* Point<Int8>(x: 3, y: 5)
+    ///     // a.x == 6; a.y == 20
+    ///     let b = Point<Int8>(x: 2, y: 10) &* Point<Int8>(x: 3, y: 50)
+    ///     // b.x == 6; b.y == -12 (after overflow)
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - lhs: The first value to multiply.
+    ///   - rhs: The second value to multiply.
+    public static func &* (lhs: Point, rhs: Point) -> Point {
+        return Point(x: lhs.x &* rhs.x, y: lhs.y &* rhs.y)
+    }
+}
+
 // MARK: Signed
 
-extension Point: Signed where Numeric: SignedNumeric {
-
-    /// Returns the given point unchanged.
-    ///
-    /// - Returns: The given point without any changes.
-    public prefix static func + (operand: Point) -> Point {
-        return Point(x: +operand.x, y: +operand.y)
-    }
-
-    /// Returns the additive inverse of the specified value.
-    ///
-    /// - Returns: The additive inverse of this value.
-    public prefix static func - (operand: Point) -> Point {
-        return Point(x: -operand.x, y: -operand.y)
-    }
+extension Point: SignedArithmetic where Numeric: SignedNumeric {
 
     /// Replaces this value with its additive inverse.
     ///
